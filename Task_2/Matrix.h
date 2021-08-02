0d727a2279e4fd887217792d551b0c41c75a75f3
Serhii Zaika <serhiizaikakbl@gmail.com>
2021-08-03 00:34:27 +0300

    Revert "Revert "Merge branch 'dev' into 'master'""
    This reverts commit 4bb818c49d1e1979d2213cf9915b891f81981f21

---
+++ Task_2/Matrix.h
@@ -0,0 +1,30 @@
+#pragma once
+#include<iostream>
+#include<cassert>
+
+class Matrix
+{
+public:
+	Matrix(); 
+	Matrix(int input_number);
+	Matrix(int** input_data, int rows, int columns);// constructor int
+	Matrix(char* input_line);
+
+	Matrix(const Matrix& copy_matrix);//copy constructor
+	Matrix(Matrix&& moving_matrix);//moving constructor
+	
+	Matrix operator=(const Matrix& copy_matrix);
+	
+	Matrix& operator=(Matrix&& moving_matrix);
+
+	void symbolMatrixRepresentation();
+	~Matrix();
+private:
+	int Rows{ 0 }, Columns{ 0 };
+	int** StorageData_int = nullptr;
+
+
+	void getMemory();
+	void verifyCharinput(const char* input_line);
+	void initData_int(int** input_data = nullptr);
+};
